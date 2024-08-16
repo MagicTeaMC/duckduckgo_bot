f7280c4156af9a9d09f2b4487bd44579e430127d
MagicTeaMC <oneowo87@gmail.com>
2024-08-17 00:26:09 +0800

    soda

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 A Discord bot that search things from DuckDuckGo.
 Use `/search` to start.
 
-[Invite Link](https://discord.com/oauth2/authorize?client_id=1274017181989994637&permissions=0&integration_type=0&scope=bot+applications.commands)
+[Invite Link](https://discord.com/oauth2/authorize?client_id=1274017181989994637&permissions=0&integration_type=0&scope=bot+applications.commands) | [Discord Server](https://discord.gg/uQ4UXANnP2)
--- core/core.py
+++ core/core.py
@@ -1,35 +1,37 @@
 import os
-import hikari
+
 import arc
+import hikari
 from ddginternal import search as ddg_search
 from definitely_typed import asyncily
 
 bot = hikari.GatewayBot(os.environ["TOKEN"])
 client = arc.GatewayClient(bot)
 
+
 @client.include
 @arc.slash_command("search", "Search something on DuckDuckGo")
-async def search_command(ctx: arc.GatewayContext,
-    question: arc.Option[str, arc.StrParams("The question to search.")],) -> None: # type: ignore
+async def search_command(
+    ctx: arc.GatewayContext,
+    question: arc.Option[str, arc.StrParams("The question to search.")]) -> None:  # type: ignore
     
     asearch = asyncily(ddg_search)
-    
+
     embed = hikari.Embed(
         title=f"Search results for: {question}",
         description="Here are the top 3 results:",
-        color=hikari.Color(0x1D4ED8)
+        color=hikari.Color(0x1D4ED8),
     )
-    
+
     for result in (await asearch(str(question))).web[:3]:
-        # Ensure the name is 256 characters or fewer
         name = f"{result.title} - {result.url}"
         if len(name) > 255:
-            name = f"{result.title[:255 - len(result.url) - 4]}... - {result.url}"
-        
+            name = f"{result.title[:(250-len(result.url))]}..."
+
         embed.add_field(
             name=name,
-            value=f"{result.description.strip()}",
-            inline=False
+            value=f"{result.description.strip()[:250]}",
+            inline=False,
         )
-    
+
     await ctx.respond(embed=embed)
