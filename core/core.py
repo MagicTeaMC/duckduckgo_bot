d0ee1f55607d0a80de892542b785bc8891028966
MagicTeaMC <oneowo87@gmail.com>
2024-08-16 23:53:15 +0800

    awd sodalao

--- core/core.py
+++ core/core.py
@@ -2,6 +2,7 @@ import os
 import hikari
 import arc
 from ddginternal import search as ddg_search
+from definitely_typed import asyncily
 
 bot = hikari.GatewayBot(os.environ["TOKEN"])
 client = arc.GatewayClient(bot)
@@ -11,7 +12,7 @@ client = arc.GatewayClient(bot)
 async def search_command(ctx: arc.GatewayContext,
     question: arc.Option[str, arc.StrParams("The question to search.")],) -> None: # type: ignore
     
-    results = ddg_search(str(question)).web[:3]
+    asearch = asyncily(ddg_search)
     
     embed = hikari.Embed(
         title=f"Search results for: {question}",
@@ -19,7 +20,7 @@ async def search_command(ctx: arc.GatewayContext,
         color=hikari.Color(0x1D4ED8)
     )
     
-    for result in results:
+    for result in (await asearch(str(question))).web[:3]:
         embed.add_field(
             name=f"{result.title} - {result.url}",
             value=f"{result.description.strip()}",
