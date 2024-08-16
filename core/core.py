7d5f8888965c4d62fb53f1e4c4d647b7e09978ec
MagicTeaMC <oneowo87@gmail.com>
2024-08-17 00:50:58 +0800

    soda

--- core/core.py
+++ core/core.py
@@ -14,7 +14,6 @@ client = arc.GatewayClient(bot)
 async def search_command(
     ctx: arc.GatewayContext,
     question: arc.Option[str, arc.StrParams("The question to search.")]) -> None:  # type: ignore
-    
     asearch = asyncily(ddg_search)
 
     embed = hikari.Embed(
@@ -24,15 +23,26 @@ async def search_command(
     )
 
     for result in (await asearch(str(question))).web[:3]:
-        name = f"{result.title} - {result.url}"
-        if len(name) > 255:
-            name = f"{result.title[:(250-len(result.url))]}..."
+        SEPARATOR = 5  # Length of " - "
+        LENGTH = len(result.url)
+
+        if LENGTH + SEPARATOR > 256:
+            truncated_url = result.url[: (256 - SEPARATOR)]
+            name = f"{result.title[:(256 - len(truncated_url) - SEPARATOR)]}... - {truncated_url}"
+        else:
+            name = f"{result.title} - {result.url}"
+            if len(name) > 256:
+                name = f"{result.title[:(256 - LENGTH - SEPARATOR)]}... - {result.url}"
+
+        if len(name) > 256:
+            name = name[:256]
 
         embed.add_field(
             name=name,
-            value=f"{result.description.strip()[:250]}",
+            value=f"{result.description.strip()}",
             inline=False,
         )
+        
         embed.set_footer(
             text = "All data are provide by DuckDuckGo and may be wrong"
         )
