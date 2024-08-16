041882b2a09d385056765576c0d85b7c4931f7f1
MagicTeaMC <oneowo87@gmail.com>
2024-08-16 23:16:50 +0800

    soda

--- core/core.py
+++ core/core.py
@@ -1,13 +1,14 @@
 import os
 import hikari
 import arc
-from ddginternal import search
+from ddginternal import search as ddg_search
 
 bot = hikari.GatewayBot(os.environ["TOKEN"])
 client = arc.GatewayClient(bot)
 
 @client.include
 @arc.slash_command("search", "Search something on DuckDuckGo")
-async def search(ctx: arc.GatewayContext,
+async def search_command(ctx: arc.GatewayContext,
     question: arc.Option[str, arc.StrParams("The question to search.")],) -> None:
-    await ctx.respond(await search(question))
+    result = await ddg_search(str(question))
+    await ctx.respond(result)
