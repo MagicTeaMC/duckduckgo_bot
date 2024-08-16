8e33f90087c6152dc87afcfa22efabc1c68e63b2
MagicTeaMC <oneowo87@gmail.com>
2024-08-16 23:41:47 +0800

    gay

--- core/core.py
+++ core/core.py
@@ -9,6 +9,21 @@ client = arc.GatewayClient(bot)
 @client.include
 @arc.slash_command("search", "Search something on DuckDuckGo")
 async def search_command(ctx: arc.GatewayContext,
-    question: arc.Option[str, arc.StrParams("The question to search.")],) -> None:
-    result = await ddg_search(str(question))
-    await ctx.respond(result)
+    question: arc.Option[str, arc.StrParams("The question to search.")],) -> None: # type: ignore
+    
+    results = ddg_search(str(question)).web[:3]
+    
+    embed = hikari.Embed(
+        title=f"Search results for: {question}",
+        description="Here are the top 3 results:",
+        color=hikari.Color(0x1D4ED8)
+    )
+    
+    for result in results:
+        embed.add_field(
+            name=f"{result.title} - {result.url}",
+            value=f"{result.description.strip()}",
+            inline=False
+        )
+    
+    await ctx.respond(embed=embed)
